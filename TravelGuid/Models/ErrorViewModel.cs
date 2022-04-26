fa18c7093a26f8827f39d99411c647d43e108ede
OO <nkwadioo@gmail.com>
2022-04-26 12:17:52 +0200

    Initial commit

---
+++ TravelGuid/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TravelGuid.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
